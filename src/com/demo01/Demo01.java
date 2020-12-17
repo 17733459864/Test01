9a15ba2de2b8598f5a5d1a62eb75be3785eabc93
17733459864 <1605457070@qq.com>
2020-12-17 16:47:40 +0800

    第一次提交

---
+++ src/com/demo01/Demo01.java
@@ -0,0 +1,12 @@
+package com.demo01;
+
+public class Demo01 {
+
+	public Demo01() {
+		// TODO Auto-generated constructor stub
+	}
+	public static void main(String[] args) {
+		System.out.println("AAA");
+	}
+
+}
